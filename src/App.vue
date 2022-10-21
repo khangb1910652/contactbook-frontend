61f94a3f032d3c45222814c458cc36b4c169197a
khangb1910652 <nguyenduykhang444@gmail.com>
2022-10-21 08:36:33 +0700

    create a contact listing page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
-<script>
+<!-- <script>
 export default {
 }
 </script>
 <template>
   <h1>Hello, Vue.js!</h1>
+</template>
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style> -->
+
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+<template>
+  <AppHeader />
+  <div class="container mt-3">
+    <router-view />
+  </div>
 </template>
 <style>
 .page {
