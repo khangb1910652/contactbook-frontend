ff7ee5a14ce52d246ac2b4e486b651a04749ecb5
khangb1910652 <nguyenduykhang444@gmail.com>
2022-10-27 18:59:03 +0700

    create add contact

--- src/views/ContactAdd.vue
+++ src/views/ContactAdd.vue
@@ -1,32 +1,45 @@
 <template>
-    <div v-if="contact" class="page">
-        <h4>Thêm Liên hệ</h4>
-        <ContactForm :contact="contact" @submit:contact="goToAddContact" />
-        <p>{{ message }}</p>
+    <div class="page">
+      <h4>Thêm liên hệ</h4>
+      <ContactForm :contact="contact" @submit:contact="onUpdateContact" />
+      <p>{{ message }}</p>
     </div>
-</template>
-<script>
-import ContactForm from '@/components/ContactForm.vue';
-import { contactService } from '@/services/contact.service';
-export default {
+  </template>
+  <script>
+  import ContactForm from "@/components/ContactForm.vue";
+  import { contactService } from "@/services/contact.service";
+  export default {
     components: {
-        ContactForm,
+      ContactForm,
+    },
+    props: {
+      contactId: { type: Number, required: false},
     },
     data() {
-        return {
-            contact: null,
-            message: '',
-        };
+      return {
+        contact: null,
+        message: "",
+      };
     },
     methods: {
-        async goToAddContact(contact) {
-            try {
-                await contactService.add(contact);
-                this.message = 'Liên hệ được thêm thành công.';
-            } catch (error) {
-                console.log(error);
-            }
-        },
+      async onUpdateContact(contact) {
+        try {
+          await contactService.create(contact);
+          this.message = "Liên hệ được thêm thành công.";
+        } catch (error) {
+          console.log(error);
+        }
+      },
     },
-};
-</script>
+    created() {
+      this.contact = {
+          name: '',
+          email: '',
+          address: '',
+          phone: '',
+          favorite: '',
+      };
+      this.message = "";
+    },
+  };
+  </script>
